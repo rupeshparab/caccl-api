ca4c43c0363bb5fc8c112138123ee19f4c1089d1
Gabe Abrams <gabeabrams@gmail.com>
2019-06-13 11:25:02 -0400

    Added page views endpoints

--- endpoints/API/User/index.js
+++ endpoints/API/User/index.js
@@ -1,5 +1,8 @@
 const EndpointCategory = require('../../../classes/EndpointCategory');
 
+const utils = require('../../common/utils');
+const prefix = require('../../common/prefix');
+
 // Import subcategories:
 const Self = require('./Self');
 
@@ -14,6 +17,36 @@ class User extends EndpointCategory {
 /*------------------------------------------------------------------------*/
 
 User.self = Self;
+
+/*------------------------------------------------------------------------*/
+/*                                Endpoints                               */
+/*------------------------------------------------------------------------*/
+
+/**
+ * Lists the page views for a user
+ * @author Gabriel Abrams
+ * @method listPageViews
+ * @memberof api.user
+ * @instance
+ * @param {object} options - object containing all arguments
+ * @param {number} options.userId - Canvas user Id
+ * @param {Date} [options.startDate] - the date to start the query for page
+ *   views
+ * @param {Date} [options.endDate] - the date to end the query for page views
+ * @return {Promise.<Object[]>} List of Canvas PageView objects {@link https://canvas.instructure.com/doc/api/users.html#PageView}
+ */
+User.listPageViews = function (options) {
+  return this.visitEndpoint({
+    path: `${prefix.v1}/users/${options.userId}/page_views`,
+    method: 'GET',
+    params: {
+      start_time: utils.includeIfDate(options.startDate),
+      end_time: utils.includeIfDate(options.endDate),
+    },
+  });
+};
+User.listPageViews.action = 'list the page views for a user';
+User.listPageViews.requiredParams = ['userId'];
 
 /*------------------------------------------------------------------------*/
 /*                                 Export                                 */
